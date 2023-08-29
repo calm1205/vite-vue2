e218e0c68f87d36693faa15dec11885f841b6761
calm1205 <naginani072@gmail.com>
2023-08-29 22:47:30 +0900

    title image to font

--- src/components/atoms/Logo.vue
+++ src/components/atoms/Logo.vue
@@ -1,9 +1,12 @@
 <script></script>
 
 <template>
-  <img src="/logo.png" alt="">
+  <h1>MNTSQ</h1>
 </template>
 
 <style lang="scss" scoped>
-
+h1 {
+  font-family: 'Taviraj', serif;
+  color: white
+}
 </style>
