5c3e3e256067ccca815fa8f4c8922b203222ba62
calm1205 <naginani072@gmail.com>
2023-08-29 09:33:24 +0900

    self-closing tag

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script>
-import { ListItem } from "@/components/atoms/index.js"
 import { Header } from "@/components/molecules/index.js"
 import { SideMenu } from "@/components/organisms/index.js"
 
 export default {
   components: {
-    ListItem,
     Header,
     SideMenu,
   },
@@ -14,13 +12,12 @@ export default {
 
 <template>
   <div class="wrapper">
-    <Header></Header>
+    <Header />
     <div class="headerArea"></div>
     <div class="underHeader">
-      <SideMenu></SideMenu>
+      <SideMenu />
       <div class="content">
         <h1>main contents</h1>
-        <ListItem name="aaa" />
       </div>
     </div>
   </div>
