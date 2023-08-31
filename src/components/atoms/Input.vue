c9bc32fc06979be62c0e2bbdd980c0a7455d07ac
calm1205 <naginani072@gmail.com>
2023-08-31 09:59:41 +0900

    input

---
+++ src/components/atoms/Input.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: ["placeholder"],
+}
+</script>
+
+<template>
+  <input :placeholder="placeholder" />
+</template>
+
+<style lang="scss" scoped>
+input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 9999px;
+  border: 1px solid $COLOR_PRIMARY;
+
+  &::placeholder {
+    color: $COLOR_PLACEHOLDER;
+    letter-spacing: 0.04rem;
+  }
+}
+</style>
--- src/components/pages/01_ConclusionVersion.vue
+++ src/components/pages/01_ConclusionVersion.vue
@@ -1,12 +1,11 @@
 <script>
-import { Card, DocumentChip } from "@/components/atoms/index.js"
-import { Card, DocumentChip } from "@/components/atoms/index.js"
-import { DocumentChip } from "../atoms"
+import { Card, DocumentChip, Input } from "@/components/atoms/index.js"
 
 export default {
   components: {
     Card,
     DocumentChip,
+    Input,
   },
 }
 </script>
@@ -17,7 +16,14 @@ export default {
     <Card text="共通" />
     <Card text="hello world hello world" />
     <DocumentChip text="契約書.pdf" />
+    <div class="inputWrap">
+      <Input placeholder="Search freely by keyword." />
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.inputWrap {
+  width: 200px;
+}
+</style>
