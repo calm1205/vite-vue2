16cc61d0c34ec5868ba598ac3252cefbcd58e27a
calm1205 <naginani072@gmail.com>
2023-09-14 08:34:50 +0900

    vuex mount

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue"
+import Vuex from "vuex"
 import App from "./App.vue"
 import router from "./router"
 import "material-icons/iconfont/material-icons.css"
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
 
 new Vue({
   el: "#app",
